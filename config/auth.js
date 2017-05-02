63aa88671baa34802bb5d7bb64933ae17b66174c
Iyvonne <mjrueb@gmail.com>
2017-05-01 20:36:54 -0400

    update missing , in url for  FB callback

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '377938202593271', // your App ID
         'clientSecret'    : 'df2185e4a884796e14bdafc4dfaa01fd', // your App Secret
-        'callbackURL'     : 'https://sandhiyoga.herokuapp.com/',
+        'callbackURL'     : 'https://sandhiyoga.herokuapp.com/auth/facebook/callback',
         // 'http://localhost:8080/auth/facebook/callback',
         'profileURL'      : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
